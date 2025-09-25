b6caee5ea34e30838c76bc3c56037279259fd128
kapeshy <atieno.patience@strathmore.edu>
2025-09-25 16:01:50 +0300

    Main pages for events

--- sendmail.php
+++ sendmail.php
@@ -16,7 +16,8 @@ require_once 'conf.php';
 function send2FACode($toEmail, $toName, $code) {
     global $conf;
 
-    $mail = new PHPMailer(true);
+    $mail = new PHPMailer(true);/*Object instantiation in OOP where PHPMailer is instantiated
+    as an oject named $mail*/
     try {
         $mail->isSMTP();
         $mail->Host       = $conf['smtp_host'];
@@ -28,16 +29,17 @@ function send2FACode($toEmail, $toName, $code) {
             : PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = $conf['smtp_port'];
 
-        $mail->setFrom($conf['smtp_user'], 'Tikika 2FA');
-        $mail->addAddress($toEmail, $toName);
+        $mail->setFrom($conf['smtp_user'], 'Tikika 2FA');//method setFrom defines the sender
+        $mail->addAddress($toEmail, $toName);//method addAddress define the recipient
 
         $mail->isHTML(true);
         $mail->Subject = 'Your Tikika 2FA Code';
         $mail->Body    = "<p>Your 2FA code is: <b>{$code}</b></p><p>This code expires in 5 minutes.</p>";
 
-        $mail->send();
+        $mail->send();//method send executes the sending of the email
         return true;
     } catch (Exception $e) {
         return false;
     }
 }
+// try and catch(Exception handling to handle errors)
